23a50985cd2501337b561fb62f449ca093728aa7
nicola <you@example.com>
2023-04-17 15:10:37 +0100

    task 4 all articles

--- src/components/ArticlesList.jsx
+++ src/components/ArticlesList.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { fetchArticles } from "./api";
 
-const ArticlesList =(articlesList, setArticlesList)=>{
+const ArticlesList =({articlesList, setArticlesList})=>{
 
     const [isLoading, setIsloading] = useState(true);
 
@@ -19,16 +19,30 @@ const ArticlesList =(articlesList, setArticlesList)=>{
       }
 return (
     <div>
-    <h2 className="articleHeader">Here are all the articles..</h2>
-    <ul>
+    <h2 className="articleHeader">Here Are All The Articles</h2>
+    <div >
+    <ul className="listboxes" >
         {articlesList.map((article) =>{
             return (
-                <li key = {article.article_id}> <img src={article.article_img_url}>
+                <li  className="articlecard" key = {article.article_id}> <img src={article.article_img_url}  alt={article.article_title}
+                width={300}
+                height={250}>
                 </img>
-                <section>
-                    {article.article_title}{article.article_id}
-
-                </section>
+               
+                <h4 > {article.title} by {article.author}
+                
+                </h4>
+                <p> Article id :{article.article_id}</p>
+               <p>
+                {article.body}
+                </p>
+               
+                
+                <p>
+                Article Votes = {article.votes}
+                </p>
+              
+                <br></br>
                 </li>
             
             
@@ -36,6 +50,7 @@ return (
             );
         })}
     </ul>
+    </div>
     </div>
    
 );
