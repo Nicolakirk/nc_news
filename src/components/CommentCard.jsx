cb76b7c129a1f1bf499560e83d9c9834d1f27bb0
nicola <you@example.com>
2023-04-18 12:21:51 +0100

    commentscard

---
+++ src/components/CommentCard.jsx
@@ -0,0 +1,16 @@
+
+const CommentCard = ({comment}) =>{
+
+return (
+    <li  className="commentcard" key = {comment.comment_id}>
+
+    <h4>Created by {comment.author}</h4>
+    <p>{comment.body}</p>
+    <p>votes = {comment.votes}</p>
+    <p></p>
+  </li>
+)
+
+}
+
+export default CommentCard;
---
+++ src/components/CommentList.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { fetchCommentsbyArticleId } from "../utils/api";
+import CommentCard from "./CommentCard";
+
+const CommentList=()=> {
+    const[isLoading, setIsLoading] =useState(true);
+    const[commentsList, setCommentsList]= useState([]);
+    const params = useParams();
+    const articleId = params.article_id;
+
+    useEffect(() => {
+        setIsLoading(true);
+        fetchCommentsbyArticleId(articleId).then((comments) => {
+          setCommentsList(comments);
+          console.log(comments);
+        setIsLoading(false);
+        });
+      }, []);
+    
+    return (
+      <div>
+        <h2>  Here are all the comments about article {articleId}</h2>
+        <ul className="commentsboxes">
+            {commentsList.map((comment)=> {
+                return ( 
+                  <CommentCard comment={comment}/>
+                )
+            })}
+        </ul>
+      </div>
+    )
+
+}
+export default CommentList;
